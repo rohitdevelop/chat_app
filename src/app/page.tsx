9ec10db366f46df41ffcf0fd72696ca78ae91c24
Rohit singh <rohitdev124421@gmail.com>
2025-12-21 20:14:39 +0530

    starting the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Home from "../components/Home";
-
-export default function page() {
-  return (
-    <div className="flex justify-center items-center min-h-screen">
-      <Home />
-    </div>
-  );
-}
+ import React from 'react'
+ 
+ const page = () => {
+   return (
+     <div>
+       <h1>hello</h1>
+     </div>
+   )
+ }
+ 
+ export default page
+ 
